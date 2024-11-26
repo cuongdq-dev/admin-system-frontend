3ef83135dade1525f5400ebee65667450f498982
dqcuong <dqcuong19@gmail.com>
2024-11-26 19:08:40 +0700

    Clean code

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,13 @@ import './i18n';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  // <StrictMode>
-  <HelmetProvider>
-    <BrowserRouter>
-      <Suspense>
-        <App />
-      </Suspense>
-    </BrowserRouter>
-  </HelmetProvider>
-  // </StrictMode>
+  <StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>
 );
