98e794a8e20e36267f9c32ed6acba10be9da63f8
dqcuong <dqcuong19@gmail.com>
2025-01-18 14:52:09 +0700

    update socket URL

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3004';
 
-export const socket = io('http://localhost:3004');
+export const socket = io('http://125.212.218.92:3004');
