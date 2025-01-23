08fc966c75fe144677debd26a755e9aae4c80fe5
dqcuong <dqcuong19@gmail.com>
2025-01-23 22:47:53 +0700

    Clean code

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,18 @@ import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import i18n from './i18n';
+import { PageLoading } from './components/loading';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <I18nextProvider i18n={i18n}>
-    <StrictMode>
-      <HelmetProvider>
-        <BrowserRouter>
-          <Suspense>
-            <App />
-          </Suspense>
-        </BrowserRouter>
-      </HelmetProvider>
-    </StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Suspense fallback={<PageLoading isLoading />}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </HelmetProvider>
   </I18nextProvider>
 );
