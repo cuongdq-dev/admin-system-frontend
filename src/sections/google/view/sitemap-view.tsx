b4c8c90751b9e1a2b96c315be33cfa6ed137b0c4
dqcuong <dqcuong19@gmail.com>
2025-03-25 15:05:52 +0700

    Update sitemap list

--- src/sections/google/view/sitemap-view.tsx
+++ src/sections/google/view/sitemap-view.tsx
@@ -1,21 +1,10 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  Chip,
-  TextField,
-  Typography,
-  useMediaQuery,
-} from '@mui/material';
+import { Box, Chip, TextField, Typography } from '@mui/material';
 import { t } from 'i18next';
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PATH_DROPDOWN, PATH_GOOGLE_SITEMAP_LIST } from 'src/api-core/path';
-import { AutocompleteComponent, AutocompleteComponentWithUrl } from 'src/components/autocomplete';
+import { AutocompleteComponentWithUrl } from 'src/components/autocomplete';
+import { Iconify } from 'src/components/iconify';
 import { TableComponent } from 'src/components/table';
 import { LanguageKey, StoreName } from 'src/constants';
 import { DashboardContent } from 'src/layouts/dashboard';
@@ -28,7 +17,7 @@ export function SiteMapListView() {
 
   const storeName = StoreName.GOOGLE_SITEMAP;
   const [siteSelect, setSiteSelect] = useState<{ id: string; title?: string } | undefined | null>(
-    new URLSearchParams(location.search)
+    new URLSearchParams(location.search).get('site_id')
       ? {
           id: new URLSearchParams(location.search).get('site_id')!,
           title: new URLSearchParams(location.search).get('site_name')!,
@@ -67,75 +56,58 @@ export function SiteMapListView() {
 
   const HeadLabel: HeadLabelProps[] = [
     {
-      id: 'site_name',
-      label: t(LanguageKey.site.nameItem),
+      id: 'path',
+      label: 'PATH',
       sort: false,
       type: 'custom',
-      align: 'center',
       width: '10%',
       render: ({ row }) => {
-        const postUrl = `${row.site_domain}/bai-viet/${row.post_slug}`;
-        const googleSearchUrl = `https://www.google.com/search?q=site:${postUrl}`;
-
         return (
           <Box display="flex" flexDirection="column">
             <Typography
-              variant="caption"
-              sx={{ cursor: 'pointer', textDecoration: 'underline', textWrap: 'nowrap' }}
-              onClick={() => window.open(postUrl, '_blank')}
+              sx={{ cursor: 'pointer', textWrap: 'nowrap' }}
+              onClick={() => window.open(row.path, '_blank')}
             >
-              {row.site_name}
-            </Typography>
-
-            <Typography
-              variant="caption"
-              sx={{ cursor: 'pointer', mt: 1, textWrap: 'nowrap' }}
-              onClick={() => window.open(googleSearchUrl, '_blank')}
-            >
-              Google Search
+              {row.path}
             </Typography>
           </Box>
         );
       },
     },
     {
-      id: 'post_title',
-      label: t(LanguageKey.site.postTitleItem),
+      id: 'lastSubmitted',
+      label: 'Submitted',
       sort: false,
       type: 'custom',
       render: ({ row }) => {
-        return <Typography>{row.post_title}</Typography>;
+        return <Typography>{fDate(row.lastSubmitted, formatStr.split.dateTime)}</Typography>;
       },
     },
 
     {
-      id: 'created_at',
-      label: t(LanguageKey.site.createdAtItem),
+      id: 'isSitemapsIndex',
+      label: 'Index',
       type: 'custom',
       align: 'center',
       sort: true,
       render: ({ row }) => {
-        return (
-          <Typography sx={{ textWrap: 'nowrap' }}>
-            {fDate(row.created_at, formatStr.dateTime)}
-          </Typography>
-        );
+        if (row.isSitemapsIndex) return <Iconify icon="ci:checkbox-checked"></Iconify>;
+        return <Iconify icon="ci:checkbox-unchecked"></Iconify>;
       },
     },
-
     {
-      id: 'indexStatus',
-      label: 'Status',
+      id: 'isPending',
+      label: 'Pending',
       type: 'custom',
       align: 'center',
+      sort: true,
       render: ({ row }) => {
-        return <IndexStatus status={row.indexStatus} />;
+        if (row.isPending)
+          return <Chip size="small" variant="outlined" label={'Pending'} color="warning" />;
+        return <Chip size="small" variant="outlined" label={'Pass'} color="primary" />;
       },
     },
   ];
-  const isMobile = useMediaQuery('(max-width:600px)');
-
-  const indexS = new URLSearchParams(location.search).get('indexStatus')!;
 
   return (
     <DashboardContent
@@ -145,8 +117,9 @@ export function SiteMapListView() {
         baseUrl={PATH_DROPDOWN + '/sites'}
         name="site_index"
         multiple={false}
+        sx={{ mb: 2 }}
         options={[]}
-        defaultValue={siteSelect || undefined}
+        defaultValue={siteSelect}
         onChange={(_, value) => {
           if (!value) {
             setSiteSelect(undefined);
@@ -170,132 +143,14 @@ export function SiteMapListView() {
         }}
       />
 
-      <Box>
-        <AutocompleteComponent
-          sx={{ my: 2 }}
-          multiple
-          disableCloseOnSelect
-          defaultValue={indexS?.split(',').map((i) => {
-            return { id: i, title: i };
-          })}
-          onChange={(event, values) => {
-            const statuValues = values.map((value: { id: string; title: string }) => {
-              return value.id;
-            });
-
-            updateUrl({ indexStatus: statuValues.toString(), limit: undefined, page: undefined });
-          }}
-          clearIcon
-          options={[
-            { title: 'NEW', id: 'NEW' },
-            { title: 'INDEXING', id: 'INDEXING' },
-            { title: 'DELETED', id: 'DELETED' },
-            { title: 'VERDICT_UNSPECIFIED', id: 'VERDICT_UNSPECIFIED' },
-            { title: 'PASS', id: 'PASS' },
-            { title: 'PARTIAL', id: 'PARTIAL' },
-            { title: 'FAIL', id: 'FAIL' },
-            { title: 'NEUTRAL', id: 'NEUTRAL' },
-          ]}
-          renderInput={(params) => {
-            return <TextField {...params} label="Filter Status" />;
-          }}
-        />
-      </Box>
-
       <TableComponent
-        component={isMobile ? 'CARD' : 'TABLE'}
+        component="TABLE"
         storeName={storeName}
         url={PATH_GOOGLE_SITEMAP_LIST}
         indexCol={true}
         refreshData={refreshData}
         headLabel={HeadLabel}
-        customCard={({ values }: { values: Record<string, any>; index: number }) => {
-          const postUrl = `${values.site_domain}/bai-viet/${values.post_slug}`;
-          const googleSearchUrl = `https://www.google.com/search?q=site:${postUrl}`;
-          return (
-            <Card sx={{ width: '100%', mb: 2 }}>
-              <CardHeader
-                avatar={<Avatar aria-label="recipe">{values.site_name?.slice(0, 1)}</Avatar>}
-                title={values.site_name}
-                subheader={fDate(values.created_at, formatStr.dateTime)}
-              />
-
-              <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Box>
-                  <Typography variant="body2">{values.site_domain}</Typography>
-                </Box>
-                <Box>
-                  <Typography variant="body2">{values.post_title}</Typography>
-                </Box>
-              </CardContent>
-              <CardActions>
-                <AutocompleteComponent
-                  size="small"
-                  defaultValue={{ title: values.indexStatus, id: values.indexStatus }}
-                  options={[
-                    { title: 'NEW', id: 'NEW' },
-                    { title: 'INDEXING', id: 'INDEXING' },
-                    { title: 'DELETED', id: 'DELETED' },
-                    { title: 'VERDICT_UNSPECIFIED', id: 'VERDICT_UNSPECIFIED' },
-                    { title: 'PASS', id: 'PASS' },
-                    { title: 'PARTIAL', id: 'PARTIAL' },
-                    { title: 'FAIL', id: 'FAIL' },
-                    { title: 'NEUTRAL', id: 'NEUTRAL' },
-                  ]}
-                  renderInput={(params) => {
-                    return <TextField {...params} margin="normal" />;
-                  }}
-                />
-              </CardActions>
-              <CardActions>
-                <Button
-                  color="error"
-                  size="medium"
-                  sx={{ textWrap: 'nowrap' }}
-                  fullWidth
-                  variant="outlined"
-                  onClick={() => window.open(postUrl, '_blank')}
-                >
-                  View Site
-                </Button>
-                <Button
-                  color="warning"
-                  size="medium"
-                  sx={{ textWrap: 'nowrap' }}
-                  fullWidth
-                  variant="outlined"
-                  onClick={() => window.open(googleSearchUrl, '_blank')}
-                >
-                  Google Search
-                </Button>
-              </CardActions>
-            </Card>
-          );
-        }}
       />
     </DashboardContent>
   );
 }
-
-const IndexStatus = ({ status }: { status?: string }) => {
-  switch (status) {
-    case 'NEW':
-      return <Chip size="small" variant="outlined" label={status} color="primary" />; // Xanh dương - mới
-    case 'INDEXING':
-      return <Chip size="small" variant="outlined" label={status} color="info" />; // Xanh nhạt - đang index
-    case 'DELETED':
-      return <Chip size="small" variant="outlined" label={status} color="default" />; // Xám - đã xóa
-    case 'PASS':
-      return <Chip size="small" variant="outlined" label={status} color="success" />; // Xanh lá - index thành công
-    case 'FAIL':
-      return <Chip size="small" variant="outlined" label={status} color="error" />; // Đỏ - lỗi
-    case 'PARTIAL':
-      return <Chip size="small" variant="outlined" label={status} color="warning" />; // Vàng - chưa hoàn chỉnh
-    case 'NEUTRAL':
-      return <Chip size="small" variant="outlined" label={status} color="secondary" />; // Tím - bị loại trừ
-    case 'VERDICT_UNSPECIFIED':
-      return <Chip size="small" variant="outlined" label={status} color="default" />; // Xám - không rõ trạng thái
-    default:
-      return <Chip size="small" variant="outlined" label={status} color="warning" />; // Mặc định vàng - không xác định
-  }
-};
